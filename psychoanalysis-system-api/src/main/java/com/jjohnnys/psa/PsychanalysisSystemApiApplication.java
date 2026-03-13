31363c87a730c1b16e08f66164062015768e848c
jjohnnys <johnnysilva85@gmail.com>
2026-03-12 21:27:25 -0300

    File ARUITETURE.MD

---
+++ psychoanalysis-system-api/src/main/java/com/jjohnnys/psa/PsychanalysisSystemApiApplication.java
@@ -0,0 +1,12 @@
+package com.jjohnnys.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychanalysisSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychanalysisSystemApiApplication.class, args);
+    }
+}
